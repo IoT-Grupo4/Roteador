411f6e368b83542d1ff4804df6fd67084fee420e
Luiz Oliveira <ziuloliveira@gmail.com>
2017-05-09 22:53:28 -0300

    basic check of sensors
    Signed-off-by: Luiz Oliveira <ziuloliveira@gmail.com>

---
+++ main.py
@@ -0,0 +1,29 @@
+from multiprocessing import Pool
+from sensor import VirtualSensor, Sensor
+
+
+def check_sensor(sensor):
+    return sensor.is_ok()
+
+
+def main():
+
+    # create virtual sensors for test
+    sensors_list = [VirtualSensor('{}'.format(x)) for x in range(10)]
+
+    # list of all sensors
+    sensors = Sensor.instances
+
+    print('Reading result of {} sensors'.format(len(sensors)))
+    with Pool() as p:
+        while True:
+            result = p.map(check_sensor, sensors)
+            if False in result:
+                pass  # TODO: altera prioridade
+                print('Not OK                ', end='\r')
+            else:
+                print('Everything is OK      ', end='\r')
+                pass  # TODO: coloca prioridade em default
+
+if __name__ == '__main__':
+    main()
